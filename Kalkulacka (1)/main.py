5cc5434038b654714db599bb531d632c498e5057
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2024-04-11 08:29:51 +0000

    Funkce

--- Kalkulacka (1)/main.py
+++ Kalkulacka (1)/main.py
@@ -4,26 +4,44 @@ while True:
   print("Napište "'konec'" při vybírání operace pro ukončení programu.")
   operace=input("Vyberte si operaci- sčítání, odčítání, násobení, dělení, umocnění, odmocnění:")
   
-
   hodnota1=float(input("Zadej první číslo:"))
   hodnota2=float(input("Zadej druhé číslo:"))
+
+  def sčítání(hodnota1, hodnota2):
+   print("Výsledek je:", hodnota1+hodnota2)
+
+  def umocnění(hodnota1, hodnota2):
+   print("Výsledek je:", hodnota1**hodnota2)
+
+  def odčítání(hodnota1, hodnota2):
+   print("Výsledek je:", hodnota1-hodnota2)
+
+  def násobení(hodnota1, hodnota2):
+    print("Výsledek je:", hodnota1*hodnota2)
   
+  def dělení(hodnota1, hodnota2):
+    print("Výsledek je:", hodnota1/hodnota2)
+
   import math
-  if operace == "odmocnění":
+
+  def odmocnění(hodnota1):
     print("Výsledek je:", math.sqrt(hodnota1))
+
+  if operace == "odmocnění":
+    odmocnění(hodnota1)
   
   if operace == "sčítání":
-    print("Výsledek je:", hodnota1+hodnota2)
+    sčítání(hodnota1, hodnota2)
   elif operace == "umocnění":
-    print("Výsledek je:", hodnota1**hodnota2)
+    umocnění(hodnota1, hodnota2)
   elif operace == "odčítání":
-    print("Výsledek je:", hodnota1-hodnota2)
+    odčítání(hodnota1, hodnota2)
   elif operace == "násobení":
-    print("Výsledek je:", hodnota1*hodnota2)
+    násobení(hodnota1, hodnota2)
   elif operace == "dělení" and hodnota2 == 0:
     print("Nulou nelze dělit")
   elif operace == "dělení":
-    print("Výsledek je:", hodnota1/hodnota2)
+    dělení(hodnota1, hodnota2)
   elif operace == "konec":
     print("Konec programu.")
     break
